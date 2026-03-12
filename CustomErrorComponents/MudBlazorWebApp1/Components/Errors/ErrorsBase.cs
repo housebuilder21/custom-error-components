d5f960bee21eedb474885f1bce36c8d9f7bbdb28
housebuilder21 <dr.delphox@gmail.com>
2026-03-12 15:21:14 -0600

    Added Visual Studio Solution

---
+++ CustomErrorComponents/MudBlazorWebApp1/Components/Errors/ErrorsBase.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace CustomErrorComponents.Components.Errors
+{
+    public class ErrorsBase : ComponentBase
+    {
+        [Parameter]
+        public string? Class { get; set; }
+
+        [Parameter]
+        public string? Style { get; set; }
+    }
+}
